361c21310f81c1611178cf8c5c4c7c76c8aa0f61
Wantedbear007 <pratapsinghbhanu444@gmail.com>
2024-02-10 14:53:41 +0530

    done.

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -5,23 +5,58 @@ const HomeScreen = ({}) => {
   // const { name, email, age, dob, contact, password } = userData;
 
   const [dataLoading, setIsLoading] = useState(true);
-  const [userData, setUserData] = useState({
+  const [userData1, setUserData] = useState({
     name: "",
     email: "",
     age: "",
     dob: "",
     contact: "",
-    password: "",
+    // password :""
   });
 
-  useEffect(async () => {
+  const getData = async () => {
     const value = localStorage.getItem("token");
     const userData = await Services.getUserDetails(value);
-    setUserData(userData)
-    console.log(userData); // Outputs: 'value'
+    setUserData(userData);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getData();
   }, []);
 
-  return <>{dataLoading ? <h1>Data is loading </h1> : <h1>hlello</h1>}</>;
+
+
+  const { name, email, age, dob, contact } = userData1;
+
+  return (
+    <>
+      {dataLoading ? (
+        <h1>Data is loading </h1>
+      ) : (
+        <div className="max-w-md mx-auto">
+          <h2 className="text-2xl font-semibold mb-4">User Information</h2>
+          <div>
+            <p className="text-gray-700">
+              <span className="font-semibold">Name:</span> {name}
+            </p>
+            <p className="text-gray-700">
+              <span className="font-semibold">Email:</span> {email}
+            </p>
+            <p className="text-gray-700">
+              <span className="font-semibold">Age:</span> {age}
+            </p>
+            <p className="text-gray-700">
+              <span className="font-semibold">Date of Birth:</span> {dob}
+            </p>
+            <p className="text-gray-700">
+              <span className="font-semibold">Contact:</span> {contact}
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default HomeScreen;
